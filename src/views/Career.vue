c14af45c9f7827dae6473e9a2aee571d406ae952
DevNoteKeeper <seoyoon.developer@gmail.com>
2025-01-20 04:26:58 +0100

    create career section

--- src/views/Career.vue
+++ src/views/Career.vue
@@ -1,11 +1,138 @@
 <template>
-    <div>
-      <h1>Welcome to the Career Page</h1>
+  <section class="career-section">
+    <div class="career-title">Career</div>
+
+    <div class="career-item">
+      <img src="@/assets/logo/companyLogo/team-sparta-logo.png" alt="Team Sparta Logo" class="company-logo"/>
+      <div class="company-info">
+        
+        <div class="company-name">Team Sparta, Seoul, Korea</div>
+      
+      <div class="position">Freelancer</div>
+      <div class="duration">2022.03 - Current</div>
+      <ul class="job-responsibilities">
+        <li>Provided Q&A tutoring for coding and no-code courses</li>
+        <li>Edited course videos using Vrew and Premiere Pro</li>
+        <li>Handled error reports from students and ensured quality corrections</li>
+      </ul>
     </div>
-  </template>
-  <script>
-  export default {
-    name: 'CareerSection',
-  };
-  </script>
-  
+  </div>
+
+    <div class="career-item">
+      <img src="@/assets/logo/companyLogo/shim-company-logo.png" alt="Shim Company Logo" class="company-logo"/>
+      <div class="company-info">
+        
+        <div class="company-name">Shim Company Korea, Seoul, Korea</div>
+      
+      <div class="position">Exhibition Assistant</div>
+      <div class="duration">2020.10 - 2021.12</div>
+      <ul class="job-responsibilities">
+        <li>Organized and planned job festival events</li>
+        <li>Created proposals for marketing and event campaigns</li>
+        <li>Managed company website and social media posts</li>
+      </ul>
+    </div>
+  </div>
+
+    <div class="career-item">
+      <img src="@/assets/logo/companyLogo/moms-touch-logo.png" alt="Mom's Touch Logo" class="company-logo"/>
+      <div class="company-info">
+        
+        <div class="company-name">Mom's Touch, Seoul, Korea</div>
+      
+      <div class="position">Manager</div>
+      <div class="duration">2019.02 - 2020.06</div>
+      <ul class="job-responsibilities">
+        <li>Managed part-time staff and scheduled shifts</li>
+        <li>Worked in kitchen, hall, and cooking areas</li>
+      </ul>
+    </div>
+  </div>
+    
+    <div class="career-item">
+      <img src="@/assets/logo/companyLogo/lucidpromo-logo.png" alt="Lucidpromo Logo" class="company-logo"/>
+      <div class="company-info">
+        
+        <div class="company-name">Lucidpromo, Seoul, Korea</div>
+      <div class="position">Marketing Concierge</div>
+      <div class="duration">2017.07 - 2018.09</div>
+      <ul class="job-responsibilities">
+        <li>Developed services for concierge in apartments</li>
+        <li>Created PowerPoint presentations for TF teams</li>
+        <li>Managed SNS accounts</li>
+      </ul>
+    </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'CareerSection',
+};
+</script>
+
+<style scoped>
+.career-section {
+  max-width: 1200px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 4rem 2rem;
+}
+
+.career-title {
+  font-size: 3rem;
+  font-weight: 700;
+  margin-bottom: 2rem;
+  text-align: left;
+}
+
+.career-item {
+  background-color: #fff;
+  padding: 2rem;
+  margin-bottom: 2rem;
+  border-radius: 8px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 3rem;
+}
+
+.company-logo {
+  width: 15rem; /* 로고 크기 조정 */
+  height: 15rem;
+  object-fit: contain;
+}
+
+.company-name {
+  font-size: 2rem;
+  font-weight: 700;
+  color: #171a35;
+}
+
+.position {
+  font-size: 1.6rem;
+  color: #4a5568;
+  margin-top: 0.5rem;
+}
+
+.duration {
+  font-size: 1.4rem;
+  color: #777;
+  margin-top: 0.5rem;
+}
+
+.job-responsibilities {
+  list-style: none;
+  margin-top: 1rem;
+  padding-left: 1rem;
+}
+
+.job-responsibilities li {
+  font-size: 1.2rem;
+  color: #333;
+  margin-bottom: 0.5rem;
+}
+</style>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,72 +1,134 @@
 <template>
-    <section class="contact-section">
+  <section class="contact-section">
+      <div class="contact-title">Contact</div>
       <div class="container">
-        <div class="contact-message">
-          <h2>Contact Me</h2>
-          <p>
-          협업 또는 프로젝트 관련 논의를 원하시면 아래 양식을 작성해 주세요. 귀하의 메세지를 소중히 다루겠습니다.
+      <div class="contact-message">
+        
+        <p>
+          협업 또는 프로젝트 관련 논의를 원하시면<br/>아래 양식을 작성해 주세요.<br/>귀하의 메세지를 소중히 다루겠습니다.
         </p>
         <p>
-          If you're interested in collaboration or discussing a project, please fill out the form below. Your message will be handled with care.
+          If you're interested in collaboration or discussing a project,<br/>please fill out the form below.<br/>Your message will be handled with care.
         </p>
-        </div>
-
-        <form @submit.prevent="sendMessage" class="contact-form">
-          <label for="name">Name</label>
-          <input type="text" id="name" v-model="form.name" placeholder="Full Name" required/>
-
-          <label for="email">Email</label>
-          <input type="email" id="email" v-model="form.email" placeholder="Email" required/>
-
-          <label for="message">Message</label>
-          <input type="message" v-model="form.message" placeholder="Message" required/>
-
-          <button type="submit">Send Message</button>
-        </form>
       </div>
-    </section>
-  </template>
-  <script>
-  export default {
-    name: 'ContactSection',
-    data(){
-      return{
-        form:{
-          name: '',
-          email: '',
-          message: '',
-        }
-      };
+
+      <form @submit.prevent="sendMessage" class="contact-form">
+        <label for="name">Name</label>
+        <input type="text" id="name" v-model="form.name" placeholder="Full Name" required />
+
+        <label for="email">Email</label>
+        <input type="email" id="email" v-model="form.email" placeholder="Email" required />
+
+        <label for="message">Message</label>
+        <textarea id="message" v-model="form.message" placeholder="Message" required></textarea>
+
+        <button type="submit">Send Message</button>
+      </form>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'ContactSection',
+  data() {
+    return {
+      form: {
+        name: '',
+        email: '',
+        message: '',
+      },
+    };
+  },
+  methods: {
+    async sendMessage() {
+      console.log('Form submitted', this.form);
+      alert('Your message has been sent!');
+      location.reload();
     },
-    methods:{
-      async sendMessage(){
-        console.log('Form submitted', this.form);
-        alert('Your message has been sent!');
-      }
-    }
-  };
-  </script>
+  },
+};
+</script>
 
 <style scoped>
-.contact-section{
-  display: flex;
-  justify-content: space-between;
-  padding: 2rem;
-}
-
-.contact-message{
-  flex:1;
-  margin-right: 2rem;
-}
-.contact-form{
-  flex:1;
+.contact-section {
   display: flex;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 4rem 0;
+  background-color: #f9f9f9;
 }
 
-.contact-form input, .contact-form textarea{
+.container {
+  display: flex;
+  flex-direction: row;
+  max-width: 1200px;
+  width: 100%;
+  gap: 3rem;
+}
+
+.contact-message {
+  text-align: left;
+}
+
+.contact-title {
+  font-size: 3rem; /* About Section과 동일한 제목 크기 */
+  font-weight: 700;
+  margin-bottom: 1.5rem;
+  text-align: left;
+}
+
+.contact-message p {
+  font-size: 1.2rem; 
+  line-height: 1.8;
   margin-bottom: 1rem;
-  padding: 0.5rem;
+}
+
+.contact-form {
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+  align-items: flex-start;
+  min-width: 50%;
+}
+
+.contact-form label {
+  font-size: 1.5rem; 
+  font-weight: 600;
+  align-self: flex-start;
+}
+
+.contact-form input,
+.contact-form textarea {
+  width: 100%;
+  max-width: 600px;
+  padding: 1rem;
+  font-size: 1.2rem; 
+  font-weight: 400;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.contact-form textarea {
+  height: 150px;
+  resize: none;
+}
+
+.contact-form button {
+  background: #171a35; /* About Section과 동일한 색상 */
+  color: #d5c8a8; /* About Section과 동일한 텍스트 색상 */
+  font-size: 1.2rem;
+  font-weight: 700;
+  padding: 1rem 2rem;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+  transition: background 0.3s ease;
+}
+
+.contact-form button:hover {
+  background: #2d3748; /* Hover 효과 추가 */
 }
 </style>
-  
