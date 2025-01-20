a7e1f8a1907a7789f6ed52032cee9f36f8ccdc42
DevNoteKeeper <seoyoon.developer@gmail.com>
2025-01-20 14:03:18 +0100

    Updates

--- vue.config.js
+++
@@ -1,6 +0,0 @@
-const { defineConfig } = require('@vue/cli-service');
-module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production' ? '/Portfolio/' : '/',
-  outputDir: 'dist',
-  transpileDependencies: true,
-});
